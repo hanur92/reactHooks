afc568d9305c07409779f15ba452e8f2d092407f
hanur92 <rkdgksdjfwkd92@gmail.com>
2020-12-14 18:27:20 +0900

    ➕ useInput

--- src/App.js
+++ src/App.js
@@ -2,7 +2,26 @@ import "./App.css";
 import React, { useState } from "react";
 import ReactDom from "react-dom";
 
+const useInput = (initValue, validator) => {
+  const [value, setValue] = useState(initValue);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
 const App = () => {
+  const maxLen = (value) => !value.includes("@");
+  const name = useInput("Mr.", maxLen);
   const [item, setItem] = useState(1);
   const increment = () => {
     setItem(item + 1);
@@ -12,9 +31,13 @@ const App = () => {
   };
   return (
     <div className="App">
+      <h1>Hello {item}</h1>
+      <h2>start editing to see some magin happen !</h2>
       <button onClick={increment}>+</button>
-      <div>{item}</div>
       <button onClick={decrement}>-</button>
+      {/* <input placeholder="Name" value={name.value} onChange={name.onChange} /> */}
+      <input placeholder="Name" {...name} />
+      {console.log({ ...name })}
     </div>
   );
 };
---
+++ src/useInput.js
@@ -0,0 +1,16 @@
+export const useInput = (initValue, validator) => {
+  const [value, setValue] = useState(initValue);
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
